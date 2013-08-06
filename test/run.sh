8c28f73d872030358e43b45812303e5a506946ed
Roy Clarkson <rclarkson@gopivotal.com>
2013-08-06 17:44:13 -0400

    Use Gradle wrapper in run script

--- test/run.sh
+++ test/run.sh
@@ -2,11 +2,11 @@ cd $(dirname $0)
 cd ../complete
 mvn clean package
 rm -rf target
-gradle build
+./gradlew build
 rm -rf build
 cd ../initial
 mvn clean package
 rm -rf target
-gradle build
+./gradlew build
 rm -rf build
 exit $ret
